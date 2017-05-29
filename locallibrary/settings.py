9bd1f85c6a8960a93a63cb4cb1270e8cae768fa4
Attila O. Szabó <oszabo@192.168.88.226>
2017-05-29 23:08:11 +0200

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
 
-ALLOWED_HOSTS =['fathomless-waters-51242.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS =['fathomless-waters-51242.herokuapp.com', '127.0.0.1', 'osabsen.pythonanywhere.com']
 
 
 
