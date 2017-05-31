30b92b7e9945176980dedf8ca21c10be636714f0
Attila O. Szabó <oszabo@192.168.88.226>
2017-05-31 23:55:14 +0200

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 
 # Static files (CSS, JavaScript, Images)
